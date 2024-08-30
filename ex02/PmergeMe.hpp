369b3149dd2c297971c7a7a63e39b010d72ca223
dabdygali <53071467+dabdygali@users.noreply.github.com>
2024-08-30 14:53:22 +0400

    in progress

---
+++ ex02/PmergeMe.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabdygal <dabdygal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 10:14:17 by dabdygal          #+#    #+#             */
+/*   Updated: 2024/08/30 10:45:32 by dabdygal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef _PMERGEME_HPP_
+	#define _PMERGEME_HPP_
+
+	#include <vector>
+
+	void	pmergeme(std::vector<int> &vec);
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabdygal <dabdygal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 12:52:23 by dabdygal          #+#    #+#             */
+/*   Updated: 2024/08/30 14:15:29 by dabdygal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+#include <cstdlib>
+#include <vector>
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+
+static void	argsToVector(const char *const *argv, std::vector<int> &vec)
+{
+	if (argv == NULL || *argv == NULL)
+		return;
+	std::stringstream	sio;
+	while (*argv != NULL)
+	{
+		sio << *argv;
+		if (*(argv + 1) != NULL)
+			sio << ' ';
+		argv++;
+	}
+	int	i;
+	while (sio.good())
+	{
+		sio >> i;
+		if (sio.fail() || i < 0)
+			throw std::invalid_argument("Error: data is not recognised as positive integer");
+		vec.push_back(i);
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	static_cast<void>(argc);
+	std::vector<int>	vec;
+	try
+	{
+		argsToVector(argv + 1, vec);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+	pmergeme(vec);
+	return EXIT_SUCCESS;
+}
